b3dafff2d5c2df48e9a81399ac5f504cf3d7bff4
miki208 <miloss208@gmail.com>
2016-06-03 13:42:39 +0200

    Add host variables

---
+++ M02/dosije.java
@@ -0,0 +1,27 @@
+package M02;
+
+public class dosije
+{
+	double prosek;
+	int indeks;
+	int id_smera;
+	java.lang.String ime;
+	java.lang.String prezime;
+	java.lang.String pol;
+	java.lang.String jmbg;
+	java.sql.Date datum_rodjenja;
+	java.lang.String mesto_rodjenja;
+	java.lang.String drzava_rodjenja;
+	java.lang.String ime_oca;
+	java.lang.String ime_majke;
+	java.lang.String ulica_stanovanja;
+	java.lang.String kucni_broj;
+	java.lang.String mesto_stanovanja;
+	java.lang.String postanski_broj;
+	java.lang.String drzava_stanovanja;
+	java.lang.String telefon;
+	java.lang.String mobilni_telefon;
+	java.lang.String email;
+	java.lang.String www_uri;
+	java.sql.Date datum_upisa;
+}
